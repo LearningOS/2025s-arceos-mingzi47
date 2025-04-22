8afb951dd7a64baf5fe8f68176a717a309a5d980
mingzi <1409429504@qq.com>
2025-04-22 21:29:58 +0800

    add slab algo

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -4,75 +4,72 @@
 #![allow(unused_variables)]
 
 use allocator::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
-use axlog::warn;
-use core::ptr::NonNull;
+use axlog::debug;
 use core::alloc::Layout;
+use core::ptr::NonNull;
 
-use rlsf::Tlsf;
-
+extern crate slab_allocator;
+use slab_allocator::Heap;
 
 pub struct LabByteAllocator {
-    inner: Tlsf<'static, u32, u32, 28, 32>,
-    total_bytes: usize,
-    used_bytes: usize,
+    inner: Option<Heap>,
 }
 
 impl LabByteAllocator {
+    /// Creates a new empty `SlabByteAllocator`.
     pub const fn new() -> Self {
-        Self {
-            inner: Tlsf::new(),
-            total_bytes: 0,
-            used_bytes: 0,
-        }
+        Self { inner: None }
+    }
+
+    fn inner_mut(&mut self) -> &mut Heap {
+        self.inner.as_mut().unwrap()
+    }
+
+    fn inner(&self) -> &Heap {
+        self.inner.as_ref().unwrap()
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        unsafe {
-            let pool = core::slice::from_raw_parts_mut(start as *mut u8, size);
-            self.inner
-                .insert_free_block_ptr(NonNull::new(pool).unwrap())
-                .unwrap();
-        }
-        self.total_bytes = size;
+        self.inner = unsafe { Some(Heap::new(start, size)) };
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        debug!("add memory : start : {}, size: {}", start, size);
         unsafe {
-            let pool = core::slice::from_raw_parts_mut(start as *mut u8, size);
-            self.inner
-                .insert_free_block_ptr(NonNull::new(pool).unwrap())
-                .ok_or(AllocError::InvalidParam)?;
+            self.inner_mut().add_memory(start, size);
         }
-        self.total_bytes += size;
         Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        warn!("alloc : {}", layout.size());
-        let ptr = self.inner.allocate(layout).ok_or(AllocError::NoMemory)?;
-        self.used_bytes += layout.size();
-        Ok(ptr)
+        debug!("alloc : {}", layout.size());
+        self.inner_mut()
+            .allocate(layout)
+            .map(|addr| unsafe { NonNull::new_unchecked(addr as *mut u8) })
+            .map_err(|_| AllocError::NoMemory)
     }
 
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        warn!("dealloc : {}", layout.size());
-        unsafe { self.inner.deallocate(pos, layout.align()) }
-        self.used_bytes -= layout.size();
+        debug!("dealloc : {}", layout.size());
+        unsafe { self.inner_mut().deallocate(pos.as_ptr() as usize, layout) }
     }
 
     fn total_bytes(&self) -> usize {
-        self.total_bytes
+        debug!("total_bytes : {}", self.inner().total_bytes());
+        self.inner().total_bytes()
     }
 
     fn used_bytes(&self) -> usize {
-        self.used_bytes
+        debug!("used_bytes : {}", self.inner().used_bytes());
+        self.inner().used_bytes()
     }
 
     fn available_bytes(&self) -> usize {
-        self.total_bytes - self.used_bytes
+        debug!("available_bytes : {}", self.inner().available_bytes());
+        self.inner().available_bytes()
     }
 }
